40cde1133665a808dec14d24fa7274be1dc796ae
Kyle Pezant <Kpez16@gmail.com>
2015-12-15 04:24:03 -0600

    Added orders pages and routed correctly

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,3 +2,12 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+
+#First Product in product table, set to variable featured_product
+class StaticPagesController < ApplicationController
+
+    def landing_page
+        @featured_product = Product.first
+    end
+
+end
