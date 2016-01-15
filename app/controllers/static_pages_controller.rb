7005c0e7a52d76d9390f820b9c3a82465d99fded
Kyle <kpez16@gmail.com>
2016-01-14 19:13:50 -0600

    Re-running migrations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,9 @@
-class StaticPagesController < ApplicationController
-  def index
-  end
-end
 
 #First Product in product table, set to variable featured_product
 class StaticPagesController < ApplicationController
 
     def landing_page
-        @featured_product = Product.first
+        @products = Product.limit(3)
     end
 
 end
