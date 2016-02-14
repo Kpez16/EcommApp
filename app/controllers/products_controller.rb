bca763e7318f13088927fb03c599071728c9d2d8
Kyle <kpez16@gmail.com>
2016-02-13 22:59:44 -0600

    Initiated search form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,18 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+        search_term = params[:q]
+        #return our filtered list here
+        if Rails.env.development?
+            @products = Product.where("name LIKE ?", "%#{search_term}%")
+        end
+        if Rails.env.production?
+            @products = Product.where("name ilike ?", "%#{search_term}%")
+        end
+    else
+        @products = Product.all
+      end
   end
 
   # GET /products/1
