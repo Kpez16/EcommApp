4b8ed5cb26d84847b2f06daaf2a8d5ad470aa7a0
Kyle <kpez16@gmail.com>
2016-03-05 18:14:39 -0600

    Added reviews/comments using raty. Styled review and product show page to show comments and average reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
-  resources :users
-  resources :products
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+    devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+    resources :users
     
-  post 'static_pages/thank_you'
+    resources :products do
+        resources :comments
+    end
+    
+    get 'static_pages/about'
 
-  get 'static_pages/index', to: 'static_pages#index'
+    get 'static_pages/contact'
+    
+    post 'static_pages/thank_you'
+
+    get 'static_pages/index', to: 'static_pages#index'
     
     resources :orders, only: [:index, :show, :create, :destroy]
 
