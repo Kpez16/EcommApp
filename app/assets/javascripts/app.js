54d1807409760524ce217587df71d177b2d92976
Kyle Pezant <kpez16@gmail.com>
2016-04-08 17:17:05 -0500

    Finished Angular app for orders. Admin can add, and delete orders.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,30 +1,35 @@
-var app = angular.module('shop', []);
+var app = angular.module('shop', ['ngResource']);
 
 $(document).on('ready page:load', function() {
     angular.bootstrap(document.body, ['shop'])
 });
 
-app.factory('models', [function(){
+app.factory('models', ['$resource', function($resource){
+    var orders_model = $resource("/orders/:id.json", {id: "@id"});
+    var products_model = $resource("/products/:id.json", {id: "@id"});
     var x = {
-        orders: []
+        orders: orders_model,
+        products: products_model
     };
     return x;
 }]);
 
-app.controller('OrdersCtrl', ['$scope', function($scope) {
+app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models) {
     //Code belonging to OrdersCtrl controller
-    $scope.orders =[
-        {id: 1, total: 24, product_id: 1, user_id: 1},
-        {id: 2, total: 7, product_id: 1},
-        {id: 3, total: 42, product_id: 1, user_id: 1}
-    ];
+    $scope.orders = models.orders.query();
+    $scope.products = models.products.query();
     //Add order
     $scope.addOrder = function() {
-        if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return;}
-        $scope.orders.push($scope.newOrder);
+        if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){return;}
+        order = models.orders.save($scope.newOrder, function(){
+            recent_order = models.orders.get({id: order.id});
+            $scope.orders.push(recent_order);
+            $scope.newOrder = '';
+        });
     };
     //Delete order
     $scope.deleteOrder = function(order) {
+        models.orders.delete(order);
         $scope.orders.splice($scope.orders.indexOf(order), 1);
-    }
+    };
 }]);
