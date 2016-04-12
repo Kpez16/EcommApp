392f7a89549c21583110deb7eadf579693a0033c
Kyle Pezant <kpez16@gmail.com>
2016-04-11 20:46:39 -0500

    Fixed issues with Redis and configured Redis correctly for production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,7 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.development?
+    $redis = Redis.new(:host => 'localhost', :port => 6379)
+end
+
+if Rails.env.production?
+    $redis = Redis.new(url: ENV["REDIS_URL"])
+end
