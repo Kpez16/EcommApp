068672fd3475d876ff8bb4245c673a79f3c21bac
Kyle Pezant <kpez16@gmail.com>
2016-04-11 20:40:36 -0500

    Adjusted application.rb file for Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,15 +24,15 @@ module EcommApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-<<<<<<< HEAD
-      if Rails.env.development?
-      config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
-      end
-=======
-          if Rails.env.production?
-    config.cache_store = :redis_store, 'REDIS_URL'
 
->>>>>>> redis
+    if Rails.env.development?
+        config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+    end
+
+    if Rails.env.production?
+        config.cache_store = :redis_store, 'REDIS_URL'
+    end
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
